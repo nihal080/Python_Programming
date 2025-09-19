26324b9da3edffc2c3555f3540d7b1fa16a65795
Mohamed Nihal <nihalbinnowfal@gmail.com>
2025-09-19 10:59:42 +0530

    Delete prg5.py

--- prg5.py
+++
@@ -1,7 +0,0 @@
-
-#perimeter of rectangle
-l = float(input("Enter the length of the rectangle : "))
-b = float(input("Enter the breadth of the rectangle : "))
-
-per=2*(l+b)
-print("perimeter of rectangle is ",per)
