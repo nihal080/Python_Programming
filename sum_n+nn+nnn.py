0c1d5dd246c12cbfbcfdd999eae3fbb89fa142ee
Mohamed Nihal <nihalbinnowfal@gmail.com>
2025-09-23 15:52:28 +0530

    23-09-2025

---
+++ sum_n+nn+nnn.py
@@ -0,0 +1,11 @@
+
+n = input("Enter an integer n: ")
+nn = int(n + n)
+nnn = int(n + n + n)
+
+print(n)
+print(nn)
+print(nnn)
+
+result = int(n) + nn + nnn
+print("The result of n + nn + nnn is:", result)
