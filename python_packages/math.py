a91f295f254ab4309759554602282823b465fa16
Mohamed Nihal <nihalbinnowfal@gmail.com>
2025-10-31 12:06:33 +0530

    Add files via upload

---
+++ python_packages/math.py
@@ -0,0 +1,7 @@
+from Math_Package.sub1.addition import add
+from Math_Package.sub2.subtraction import subtract
+
+x = 5
+y = 10
+print("addition:", add(x,y))
+print("subtraction : " , subtract(x,y))
